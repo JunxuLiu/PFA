3a6094db10a92e57183f2d1c775adc0082eacb42
liujunxu <2017000608@ruc.edu.cn>
2021-05-01 15:21:19 +0800

    add delay property

--- run/run_cnn_wavg.sh
+++ run/run_cnn_wavg.sh
@@ -18,10 +18,10 @@ dataset='mnist'
 ########## cnn wavg/fedavg ###########
 
 for element in ${distributions[@]}; do {
-for i in 30 40 50; do {
+for i in 20; do {
 for learning_rate in 0.001 0.005; do {
 
-# cnn-noniid-bs4
+# cnn-iid-bs4
 log_dir=${log_path}'/log_'$vid'/'${dataset}'/cnn/iid/'${element}
 if [ ! -d $log_dir ]
 then
@@ -31,9 +31,7 @@ echo $log_dir'-'$i
 
 nohup python main.py --max_steps 10000 --dataset $dataset --model cnn --lr $learning_rate --N $i --client_dataset_size ${dataset_size} --num_microbatches ${batch_size} --client_batch_size ${batch_size} --sample_mode R --sample_ratio 0.8 --local_steps $local_steps --dpsgd True --eps $element --weiavg True --version $vid --save_dir ${save_dir} >${log_dir}/${i}_bs${batch_size}_nm${batch_size}_10000_${local_steps}_R8_wavg_constlr${learning_rate} 2>&1 &
 
-#nohup python main.py --max_steps 10000 --dataset $dataset --model cnn --lr $learning_rate --N $i --noniid True --noniid_level ${noniid_level} --client_dataset_size ${dataset_size} --num_microbatches ${batch_size} --client_batch_size ${batch_size} --sample_mode R --sample_ratio 0.8 --local_steps 100 --dpsgd True --eps $element --fedavg True --version $vid --save_dir ${save_dir} >${log_dir}/${i}_bs${batch_size}_nm${batch_size}_10000_100_R8_fedavg_constlr${learning_rate} 2>&1 &
-
-# cnn-iid
+# cnn-noniid
 log_dir=${log_path}'/log_'$vid'/'${dataset}'/cnn/noniid'${noniid_level}'/'${element}
 if [ ! -d $log_dir ]
 then
@@ -42,8 +40,6 @@ fi
 echo $log_dir'-'$i
 
 nohup python main.py --max_steps 10000 --dataset $dataset --model cnn --lr $learning_rate --N $i --client_dataset_size ${dataset_size} --noniid True --noniid_level ${noniid_level} --num_microbatches ${batch_size} --client_batch_size ${batch_size} --sample_mode R --sample_ratio 0.8 --local_steps ${local_steps}  --dpsgd True --eps $element --weiavg True --version $vid --save_dir ${save_dir} >${log_dir}/${i}_bs${batch_size}_nm${batch_size}_10000_${local_steps}_R8_wavg_constlr${learning_rate} 2>&1 &
-
-#nohup python main.py --max_steps 10000 --dataset $dataset --model cnn --lr $learning_rate --N $i --client_dataset_size ${dataset_size} --num_microbatches ${batch_size} --client_batch_size ${batch_size} --sample_mode R --sample_ratio 0.8 --local_steps 100  --dpsgd True --eps $element --fedavg True --version $vid --save_dir ${save_dir} >${log_dir}/${i}_bs${batch_size}_nm${batch_size}_10000_100_R8_fedavg_constlr${learning_rate} 2>&1 &
 
 } done 
 wait
